5491426166a8a26916fa3ae082fbf5d7234bb787
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-09-27 17:21:48 -0300

    Initial Commit

---
+++ dumpNotifications/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dumpNotifications
+//
+//  Created by Marcelo on 9/27/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dumpNotifications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dumpNotifications
+//
+//  Created by Marcelo on 9/27/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
